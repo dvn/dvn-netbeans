2e4bd8d5e95e225558328e293665b109dc6deb63
Philip Durbin <philip_durbin@harvard.edu>
2013-04-03 14:14:11 -0400

    download icefaces

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 curl -L -O http://download.netbeans.org/netbeans/7.2.1/final/zip/netbeans-7.2.1-201210100934-ml.zip
 curl -L -O http://download.java.net/glassfish/3.1.2.2/release/glassfish-3.1.2.2.zip
+curl -L -O http://dvn.iq.harvard.edu/dev/icefaces/ICEfaces-2.0.2-Netbeans-7.0.zip

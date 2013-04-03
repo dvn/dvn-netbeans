12f620c899761fcdae8e89ff12ad5cdac11812b2
Philip Durbin <philip_durbin@harvard.edu>
2013-04-03 10:50:52 -0400

    download glassfish too

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 curl -L -O http://download.netbeans.org/netbeans/7.2.1/final/zip/netbeans-7.2.1-201210100934-ml.zip
+curl -L -O http://download.java.net/glassfish/3.1.2.2/release/glassfish-3.1.2.2.zip
